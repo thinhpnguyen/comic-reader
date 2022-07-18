37a736efe89360b5f2a4ec726cedc284dc4d6c41
Thinh Nguyen <thinhnguyen@Thinhs-MacBook-Pro.local>
2022-07-18 00:43:08 -0700

    fix issue with drop files not displaying

--- scripts/display.js
+++ scripts/display.js
@@ -1,9 +1,9 @@
-function display(mode, currentPage, files) {
+function display(mode, currentPage, incrementCurrentPage, files) {
   switch (mode) {
     case "single":
       break;
     case "double":
-      displayDoubly(currentPage, files);
+      displayDoubly(currentPage, incrementCurrentPage, files);
       break;
     case "conti":
       displayContinuously(files);
@@ -29,25 +29,25 @@ function styleImage(img) {
   img.className = "page";
   img.draggable = "false";
 }
-async function loadImage(elem, file) {
-  //synchronous function
-  //synchronously
-  return new Promise((resolve, reject) => {
-    file.async("blob").then(
-      // get file data as a blob
-      function (blob) {
-        elem.onload = () => resolve(elem);
-        elem.onerror = reject;
-        elem.className = "page";
-        elem.draggable = "false";
-        elem.src = URL.createObjectURL(blob);
-      },
-      function (err) {
-        console.log(err);
-      }
-    );
-  });
-}
+// async function loadImage(elem, file) {
+//   //synchronous function
+//   //synchronously
+//   return new Promise((resolve, reject) => {
+//     file.async("blob").then(
+//       // get file data as a blob
+//       function (blob) {
+//         elem.onload = () => resolve(elem);
+//         elem.onerror = reject;
+//         elem.className = "page";
+//         elem.draggable = "false";
+//         elem.src = URL.createObjectURL(blob);
+//       },
+//       function (err) {
+//         console.log(err);
+//       }
+//     );
+//   });
+// }
 function displayContinuously(files) {
   // continuous scroll
   //code credit: https://github.com/dduponchel
@@ -62,20 +62,20 @@ function displayContinuously(files) {
   });
 }
 
-function displayDoubly(currentPage, files) {
+function displayDoubly(currentPage, incrementCurrentPage, files) {
   // double page
 
-  console.log(currentPage);
   //only display 1 page for the cover
   //and if the last page is odd then only display 1 page
-  let k = currentPage.val === files.length - 1 || currentPage.val === 0 ? 0 : 1;
+  let k = currentPage === files.length - 1 || currentPage === 0 ? 0 : 1;
 
-  let startPoint = currentPage.val;
+  let startPoint = currentPage;
   for (let i = 0; i <= k; i++) {
     if (startPoint < 0) return; // handle the case when cover is the only page left
     let div = intializeImageContainer("doublePageContainer", "ltr");
     // the images loaded synchronously to check if first image is double page, if so then only load one;
     let img = files[startPoint + i];
+    console.log(img);
     styleImage(img);
     //if this is doubled page but is a second page, then not display
     if (img.width > img.height && i === 1) {
@@ -83,11 +83,11 @@ function displayDoubly(currentPage, files) {
     } else if (img.width > img.height) {
       div.className = "singlePageContainer";
       div.append(img);
-      currentPage.val++;
+      incrementCurrentPage();
       break;
     } else {
       div.append(img);
-      currentPage.val++;
+      incrementCurrentPage();
     }
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,13 @@
 
   let prevEn = 0;
   let images; //hold images so they remain the same when switching modes
-  let currentPage = { val: 0 }; //keep track of current page in double page mode
+  let currentPage = 0; //keep track of current page in double page mode
   let mode = "double";
-  let keys;
+  let files;
+
   function reset() {
     firstPage = true;
-    currentPage.val = input.files.length - 1; // initialize current page for every new files read in
+    currentPage = files.length - 1; // initialize current page for every new files read in
     let pages = document.querySelector(".pages");
     pages.innerHTML = "";
   }
@@ -51,96 +52,34 @@
       });
     return res;
   }
-  async function handleFiles(files) {
+  function handleFiles(files) {
     const inputContainer = document.querySelector(".inputContainer");
     inputContainer.setAttribute("style", "display:none");
     inputContainer.style.display = "none";
-    {
-      // continuous scroll
-      /*if (mode === "continuous") {
-      //choosing file in Finder from bottom up make the first file becomes last in Filelist
-      //can be fixed if let user open cbz instead
-      for (let i = files.length - 1; i >= 0; i--) {
-        let div = document.createElement("div");
-        div.className = "continousPageContainer";
-        let reader = new FileReader();
-        document.querySelector(".pages").append(div); // place holder div
-        // the images loaded asynchronously will be place to their respective divs, so they will be in order
-        reader.onload = function () {
-          const img = new Image();
-          img.src = reader.result;
-          img.className = "page";
-          div.appendChild(img);
-        };
-
-        reader.readAsDataURL(files[i]);
-      }
-    } else {
-      // double page
-
-      //only display 1 page for the cover
-      //and if the last page is odd then only display 1 page
-      let k = firstPage || currentPage === 0 ? 0 : 1;
-
-      let i = 0;
-      let startPoint = currentPage;
-      while (i <= k) {
-        if (startPoint < 0) return; // handle the case when cover is the only page left
-        let div = document.createElement("div");
-        div.className = "doublePageContainer";
-        document.querySelector(".pages").prepend(div); // place holder div
-        // the images loaded synchronously to check if first image is double page, if so then only load one;
-        let width;
-        const img = new Image();
-        const imgSrc = await readFileAsDataURL(files[startPoint - i]);
-        await loadImage(imgSrc, img);
-        img.className = "page";
-        img.draggable = "false";
-        width = img.width;
-        if (startPoint === files.length - 1 && coverWidth === undefined) {
-          coverWidth = width;
-          div.appendChild(img);
-          currentPage--;
-          break;
-        }
-        //if this is doubled page but is a second page, then not display
-        if (width > coverWidth + 10 && i === 1) {
-          break;
-        } //10 is an adjustment
-        else if (width > coverWidth) {
-          div.className = "singlePageContainer";
-          div.appendChild(img);
-          currentPage--;
-          break;
-        }
-        div.appendChild(img);
-        currentPage--;
-        i++;
-      }
-    }*/
-    }
     let zip = new JSZip();
     zip.loadAsync(files[0]).then(
       function (zip) {
         images = getValidImageKeys(zip.files); //get keys of valid images to be display
-        display(mode, currentPage, images);
+        console.log(images);
+        display(mode, currentPage, incrementCurrentPage, images);
       },
       function (e) {
         console.log(e);
       }
     );
-    firstPage = false;
   }
   //display a page as single
 
+  function incrementCurrentPage() {
+    ++currentPage;
+  }
   function flip() {
-    console.log(mode);
     if (mode !== "double" && mode !== "single") return;
     const container = document.querySelector(".pages");
     if (container) {
       prevEn += 1;
       container.innerHTML = "";
-      display(mode, currentPage, images);
+      display(mode, currentPage, incrementCurrentPage, images);
     }
   }
 
@@ -158,11 +97,11 @@
   function previous() {
     const container = document.querySelector(".pages");
     if (container) {
-      currentPage += 4; //add 4 because after calling handleFiles() currentPage is incremented by for;
-      if (currentPage >= files.length) reset();
+      currentPage -= 4;
+      if (currentPage < 0) reset();
       prevEn -= 1;
       container.innerHTML = "";
-      handleFiles(files);
+      display(mode, currentPage, incrementCurrentPage, images);
     }
   }
 
@@ -170,8 +109,9 @@
   input.addEventListener(
     "change",
     function (e) {
-      reset();
       files = input.files;
+      reset();
+      console.log(files);
       handleFiles(files);
     },
     false
@@ -182,7 +122,7 @@
   });
 
   pages.addEventListener("click", function (e) {
-    if (currentPage.val < 0) return;
+    if (currentPage < 0) return;
     flip();
   });
 
@@ -203,7 +143,7 @@
         console.log("some thing is wrong with the mode");
     }
     reset();
-    display(mode, currentPage, images);
+    display(mode, currentPage, incrementCurrentPage, images);
   });
 
   dropArea.addEventListener(
@@ -219,8 +159,11 @@
     function (e) {
       e.preventDefault();
       e.stopPropagation();
+
+      files = e.dataTransfer.files;
       reset();
-      handleFiles(e.dataTransfer.files);
+      console.log(files);
+      handleFiles(files);
     },
     false
   );
